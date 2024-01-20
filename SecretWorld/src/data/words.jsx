15cfe888ee04b3ec15b91375f1aca8f9b1dd7441
lanassilva0 <lanassilvalss@gmail.com>
2024-01-19 21:36:31 -0300

    commit inicial novo projeto

---
+++ SecretWorld/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Melância", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cerébro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Microfone"],
+    programação: ["Linguagem", "Framework", "React", "JavaScript", "TypeScript"],
+    alimento: ["Arroz", "Macarrão", "Carne", "Ovo", "Leite"]
+};
