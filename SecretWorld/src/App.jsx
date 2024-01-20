368c149b2ade43bb585394496d1911f8ad8d7f05
lanassilva0 <lanassilvalss@gmail.com>
2024-01-20 00:20:17 -0300

    criação dos componentes de cada estágio do jogo

--- SecretWorld/src/App.jsx
+++ SecretWorld/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
 
+//react
+import { useState, useCallback, useEffect } from 'react'
+
+//CSS
 import './App.css'
-import StartScreen from './assets/component/StartScreen'
+
+//data
+import {wordsList} from './data/words'
+
+//components
+import StartScreen from './component/StartScreen'
+import Game from './component/Game'
+import GameOver from './component/GameOver'
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name:"game"},
+  {id: 3, name: "end"}
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
   return (
     <>
-      <StartScreen />
+      {gameStage == 'start' && <StartScreen />}
+      {gameStage == 'game' && <Game />}
+      {gameStage == 'end' && <GameOver />}
     </>
   )
 }
