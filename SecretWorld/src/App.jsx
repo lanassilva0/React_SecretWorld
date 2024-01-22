d3aa992400059a9ef32d721ddfc6f20cdc40013e
lanassilva0 <lanassilvalss@gmail.com>
2024-01-22 01:25:54 -0300

    condição de vitória e useCallBack

--- SecretWorld/src/App.jsx
+++ SecretWorld/src/App.jsx
@@ -1,4 +1,3 @@
-
 //react
 import { useState, useCallback, useEffect } from 'react'
 
@@ -33,7 +32,7 @@ function App() {
   const [guesses, setGuesses] = useState(3)
   const [score, setScore] = useState(0)
 
-  const pickTheWordAndCategory = () =>{
+  const pickWordAndCategory = useCallback(() =>{
 
     const categories = Object.keys(words)
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
@@ -41,12 +40,14 @@ function App() {
     const word = words[category][Math.floor(Math.random() * words[category].length)]
 
     return [word,category]
-  }
+  }, [words])
 
-  //start the game
-  const startGame = () => {
+  
+  const startGame = useCallback(() => {
 
-    const [word, category] = pickTheWordAndCategory();
+    clearLettersStates()
+
+    const [word, category] = pickWordAndCategory();
 
     let wordLetters = word.split("");
     wordLetters = wordLetters.map((l) => l.toString().toLowerCase()); 
@@ -56,9 +57,9 @@ function App() {
     setLetters(wordLetters)
 
     setGameStage(stages[1].name)
-  }
-
-  //process the letter input
+  }, [pickWordAndCategory])
+  
+  
   const verifyLetter = (letter) => {
     
     const normalizedLetter = letter.toLowerCase();
@@ -92,6 +93,16 @@ function App() {
       setGameStage(stages[2].name);
     }
   }, [guesses]);
+
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)];
+    
+    if (guessedLetters.length === uniqueLetters.length) {
+      setScore((actualScore) => (actualScore += 100))
+      startGame()
+    }
+  },  [guessedLetters, letters, startGame])
 
   const retry = () => {
     setScore(0)
